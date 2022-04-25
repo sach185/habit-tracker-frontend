94719bf934238e2ff79d2124ed466234fbbb2789
Sachin <sachinn185@gmail.com>
2022-04-25 20:38:13 +0530

    dev server url

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -1,3 +1,3 @@
 export const PROD_SERVER_URL = "https://habit-tracker-demo.herokuapp.com";
 export const DEV_SERVER_URL = "http://localhost:8080";
-export const SERVER_URL = PROD_SERVER_URL;
+export const SERVER_URL = DEV_SERVER_URL;
