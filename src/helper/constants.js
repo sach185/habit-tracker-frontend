15c0af9dcdf28fadb88ded59278f954f41f2866f
Sachin <sachinn185@gmail.com>
2024-02-14 00:32:55 +0530

    api endpt update

--- src/helper/constants.js
+++ src/helper/constants.js
@@ -1,3 +1,3 @@
-export const PROD_SERVER_URL = "https://habit-tracker-demo.herokuapp.com";
+export const PROD_SERVER_URL = "https://wild-red-cocoon-shoe.cyclic.app/";
 export const DEV_SERVER_URL = "http://localhost:8080";
 export const SERVER_URL = DEV_SERVER_URL;
